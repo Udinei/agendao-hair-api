6e155ca163ff1a274d5c5ffd2143af2d5ed3c57c
Udinei Silva <ukaliko@gmail.com>
2019-12-05 23:38:31 -0400

    Criação inicial do projeto

---
+++ src/main/java/com/javaus/AgendaoApplication.java
@@ -0,0 +1,13 @@
+package com.javaus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaus/AgendaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
